7fa89e3f49e34fe028e50535ef7d554e2b12e37e
MaciejMCh <maciejmch@gmail.com>
2021-11-05 15:00:51 +0100

    Update SwiftDocumentScannerFlutterPlugin.swift

--- ios/Classes/SwiftDocumentScannerFlutterPlugin.swift
+++ ios/Classes/SwiftDocumentScannerFlutterPlugin.swift
@@ -3,27 +3,9 @@ import UIKit
 
 public class SwiftDocumentScannerFlutterPlugin: NSObject, FlutterPlugin {
     
-    var rootViewController: UIViewController?
-    var result: FlutterResult?
-    
-    
-    public override init() {
-        super.init()
-        rootViewController =
-            (UIApplication.shared.delegate?.window??.rootViewController)!;
-    }
-    
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: Utils.channelName, binaryMessenger: registrar.messenger())
-        let instance = SwiftDocumentScannerFlutterPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-    }
+    public static func register(with registrar: FlutterPluginRegistrar) {}
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         result(FlutterMethodNotImplemented)
     }
-    
-    private func camera() {}
-    
-    func gallery() {}
 }
