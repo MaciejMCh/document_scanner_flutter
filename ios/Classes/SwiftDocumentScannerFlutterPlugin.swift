a042b838ba64ef3d3cd7afbb8fd4dd32c4c2f525
MaciejMCh <maciejmch@gmail.com>
2021-11-05 14:39:06 +0100

    Update SwiftDocumentScannerFlutterPlugin.swift

--- ios/Classes/SwiftDocumentScannerFlutterPlugin.swift
+++ ios/Classes/SwiftDocumentScannerFlutterPlugin.swift
@@ -1,4 +1,3 @@
-import WeScan
 import Flutter
 import UIKit
 
@@ -21,66 +20,10 @@ public class SwiftDocumentScannerFlutterPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        self.result = result
-        typealias channelMethod = () -> ()
-        var channelMethods : Dictionary = [String : channelMethod]()
-        channelMethods["camera"] = camera
-        channelMethods["gallery"] = gallery
-        if(!channelMethods.keys.contains(call.method)){
-            result(FlutterMethodNotImplemented)
-        }
-        
-        channelMethods[call.method]!();
-        
+        result(FlutterMethodNotImplemented)
     }
     
-    private func camera(){
-        let scannerViewController: ImageScannerController = ImageScannerController()
-        scannerViewController.imageScannerDelegate = self
-        
-        rootViewController?.present(scannerViewController, animated:true, completion:nil)
-    }
+    private func camera() {}
     
-    func gallery() {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        rootViewController?.present(imagePicker, animated: true)
-    }
-}
-
-extension SwiftDocumentScannerFlutterPlugin : ImageScannerControllerDelegate{
-    
-    public func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
-        scanner.dismiss(animated: true)
-        let path = Utils.getScannedFile(results: results)
-        result?(path)
-    }
-    
-    public func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
-        scanner.dismiss(animated: true)
-    }
-    
-    public func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
-        scanner.dismiss(animated: true)
-    }
-}
-
-extension SwiftDocumentScannerFlutterPlugin: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true)
-    }
-    
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        picker.dismiss(animated: true)
-        
-        guard let image = info[.originalImage] as? UIImage else { return }
-        pikedCamera(image: image)
-    }
-    
-    private func pikedCamera(image: UIImage? = nil){
-        let scannerViewController: ImageScannerController = ImageScannerController(image: image)
-        scannerViewController.imageScannerDelegate = self
-        rootViewController?.present(scannerViewController, animated:true, completion:nil)
-    }
+    func gallery() {}
 }
